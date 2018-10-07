31bf5a47c3ca4b0bd89cef480eb90f984e9ec882
Andrzej Gajewski <gajewskiandrzej@hotmail.com>
2018-10-07 15:14:51 +0200

    strona_48

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View ("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
